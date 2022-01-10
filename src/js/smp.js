8e673bc996b08d795cd975b341be97ea188d6ce6
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-01-10 23:49:21 +0200

    Add simplelightbox

---
+++ src/js/smp.js
@@ -0,0 +1,8 @@
+var lightbox = new SimpleLightbox('.gallery a', {
+  //   overlay: true,
+  captions: false,
+});
+
+// var gallery = $('.gallery a').simpleLightbox();
+
+// gallery.next(); // Next Image
