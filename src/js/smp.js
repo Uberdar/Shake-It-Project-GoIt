bb2fa6a5b38ec59ef7a2d7ae8e0554e4e6e45b6b
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-01-11 13:38:01 +0200

    Styled gallery

--- src/js/smp.js
+++ src/js/smp.js
@@ -1,9 +1,1 @@
-var lightbox = new SimpleLightbox('.gallery a', {
-  //   overlay: true,
-  gallery: simple - lightbox,
-  captions: false,
-});
-
-// var gallery = $('.gallery a').simpleLightbox();
-
-// gallery.next(); // Next Image
+var lightbox = new SimpleLightbox('.gallery a', {});
