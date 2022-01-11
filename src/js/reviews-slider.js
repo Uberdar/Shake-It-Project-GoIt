5b40861bd7b4adf01d58729c9cf9cb5534a8a493
culozik <eugenbah@gmail.com>
2022-01-11 21:48:52 +0200

    Доработан слайдер секции reviews. Изменён размер паддингов.
    К слайдеру добавлено:
    1. автопрокрутку;
    2. навигацию по слайдеру с помощью клавиатуры и мыши;
    Изменена скорость смены слайдов.

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -1,7 +1,24 @@
 var swiper = new Swiper('.mySwiper', {
-        loop: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      });
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+  },
+  grabCursor: true,
+  keyboard: {
+    enable: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    invert: true,
+    forceToAxis: false,
+    sensitivity: 1,
+  },
+  speed: 800,
+});
